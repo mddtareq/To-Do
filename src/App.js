ec40f08410295671784800a44c7171196744a7d1
Mohammad Tareq <mdtareq320@gmail.com>
2020-10-25 12:23:51 +0600

    beta testing

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-import Active from './components/Active/Active';
 import Add from './components/Add/Add';
-import All from './components/All/All';
-import Done from './components/Done/Done';
-import MenuItem from './components/MenuItem/MenuItem';
 
 function App() {
   return (
-    <>
       <Add></Add>
-      <Router>
-        <MenuItem></MenuItem>
-        <Switch>
-          <Route path='/all'>
-            <All></All>
-          </Route>
-          <Route path='/active'>
-            <Active></Active>
-          </Route>
-          <Route path='/done'>
-            <Done></Done>
-          </Route>
-        </Switch>
-      </Router>
-    </>
   );
 }
 
--- src/components/Add/Add.js
+++ src/components/Add/Add.js
@@ -1,50 +1,110 @@
 import React, { useEffect, useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
-import { useHistory } from 'react-router-dom';
+import Todo from '../Todo/Todo';
 import './Add.css'
 
 const Add = () => {
 
-    const [newTodo,setNewTodo] = useState('');
-    const history = useHistory();
-    const [todo,setTodo] = useState([]);
+    const [newTodo, setNewTodo] = useState('');
+    const [todo, setTodo] = useState([]);
+    const [filter, setFilter] = useState([]);
+    const [category, setCategory] = useState('all');
 
-    useEffect(() =>{
-        const list =localStorage.getItem('todo');
+
+    useEffect(() => {
+        const list = localStorage.getItem('todo');
         list && setTodo(JSON.parse(list));
-    },[todo])
+    }, [])
 
-    const submit=(event) => {
-        if(newTodo.length>0){
-            const createTodo={
-            name:newTodo,
-            isActive: true,
-            id:new Date().getTime()+Math.floor(Math.random() * 1000)
+    const submit = (event) => {
+        if (newTodo.length > 0) {
+            const createTodo = {
+                name: newTodo,
+                isActive: true,
+                id: new Date().getTime() + Math.floor(Math.random() * 1000)
+            }
+            const addTodo = [...todo, createTodo]
+            setTodo(addTodo);
+            localStorage.setItem('todo', JSON.stringify(addTodo));
+            setNewTodo('');
+            document.getElementById('task-text').value = '';
         }
-        const addTodo=[...todo,createTodo]
-        setTodo(addTodo);
-        localStorage.setItem('todo',JSON.stringify(addTodo));
-        setNewTodo('');
-        document.getElementById('task-text').value='';
-        }
-        else{
+        else {
             alert("Please Enter Some Data");
         }
-       
     }
 
     const change = (event) => {
-        const newTask=event.target.value;
+        const newTask = event.target.value;
         setNewTodo(newTask);
+    }
+
+
+    useEffect(() => {
+        if (category === 'all') {
+            return setFilter([...todo])
+        }
+        if (category === 'done') {
+            const currentTodo = todo.filter(item => item.isActive !== true)
+            return setFilter([...currentTodo])
+        }
+        if (category === 'active') {
+            const currentTodo = todo.filter(item => item.isActive === true)
+            return setFilter([...currentTodo])
+        }
+    }, [todo, category])
+
+    const setDone = (id) => {
+        const done = todo.map(todo => {
+            if (todo.id === id) {
+              todo.isActive = !todo.isActive;
+            }
+            return todo;
+          });
+          localStorage.setItem('todo', JSON.stringify(done));
+          setTodo(done)
+    }
+
+    const setTrash = (id) => {
+        const trash = todo.filter(todo => todo.id !== id)
+        localStorage.setItem('todo', JSON.stringify(trash))
+        setTodo(trash)
+    }
+
+    const setUndo = (id) => {
+        const undo=todo.filter(todo =>{
+            if(todo.id===id){
+                todo.isActive=!todo.isActive;
+            }
+            return todo;
+        }); 
+        localStorage.setItem('todo', JSON.stringify(undo));
+        setTodo(undo)
     }
 
     return (
         <Container>
             <Row>
-                <Col xs={12} sm={{span:10,offset:1}} md={{span:8,offset:2}} lg={{span:6,offset:3}} xl={{span:6,offset:3}}>
-                <h3 className="text-center mt-4 mb-3 heading" >Add Your Task</h3>
+                <Col xs={12} sm={{ span: 10, offset: 1 }} md={{ span: 8, offset: 2 }} lg={{ span: 6, offset: 3 }} xl={{ span: 6, offset: 3 }}>
+                    <h3 className="text-center mt-4 mb-3 heading" >Add Your Task</h3>
                     <input id='task-text' onChange={change} className="from-control" type="text" required />
-                    <input onClick={submit} className="submit-button" type="submit" value="Add"/>
+                    <input onClick={submit} className="submit-button" type="submit" value="Add" />
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={12} sm={{ span: 10, offset: 1 }} md={{ span: 8, offset: 2 }} lg={{ span: 6, offset: 3 }} xl={{ span: 6, offset: 3 }}>
+                    <div className="custom-nav text-center mt-3">
+                        <button onClick={() => setCategory('all')} className="mr-3" to="/all">All</button>
+                        <button onClick={() => setCategory('active')} className="mr-3" to="/active">Active</button>
+                        <button onClick={() => setCategory('done')} className="mr-3" to="/done">Done</button>
+                    </div>
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={12} sm={{ span: 10, offset: 1 }} md={{ span: 8, offset: 2 }} lg={{ span: 6, offset: 3 }} xl={{ span: 6, offset: 3 }}>
+                    {
+                        filter.map(todo => <Todo setUndo={setUndo} setTrash={setTrash} setDone={setDone} key={todo.id} todo={todo}></Todo>)
+                    }
                 </Col>
             </Row>
         </Container>
